e42113b2889c59bb9b64efb4899dc3763c68a7ac
Jason Lin <jason.lin.sjtu@gmail.com>
2018-02-23 08:53:25 +0000

    basic tree implementation

--- tree_util.py
+++ tree_util.py
@@ -5,14 +5,48 @@ Operations includes:
 
 1. search node
 
-author: JL
+author: JL <linjiach@usc.edu>
 '''
 
-class Tree:
+class Node:
+    def __init__(self, label, children=[]):
+    	'''
+		The semantic role labeling format requires 8 fields.
+		ID: current token ID (starting at 1).
+		FORM: word form.
+		LEMMA: lemma.
+		POS: part-of-speech tag.
+		FEATS: features (different features are delimited by '|', keys and values are delimited by '=', and '_' indicates no feature).
+		HEAD: head token ID.
+		DEPREL: dependency label.
+		SHEADS: semantic heads ('_' indicates no semantic head).
+		'''
+        self.label = label # A dictionary containing 8 fields (optional)
+        self.children = children
+        
+    def add_children(self, children):
+    	self.children.extend(children)
+    	
+    def find_child(self, field, value):
+    	for child in self.children:
+    		if child.label.get(field) == value:
+    			return child
+    	return None
+    
+    def del_child(self, field, value):
+    	for i, child in enumerate(self.children):
+    		if child.label.get(field) == value:
+    			del self.children[i]
+    			return
 
-	def __init__(self):
-		pass
 
-
-def search_node(root, name):
+def search_lemmas(node, lemmas):
+	'''
+	Args:
+		node: The root node for searching. Node
+		lemmas: All the target lemmas. List of string
+		
+	Returns:
+		All sub nodes that has one of the given lemmas.
+	'''
 	pass
--- tree_util_test.py
+++ tree_util_test.py
@@ -4,10 +4,22 @@ Unit tests for tree_util.py.
 '''
 
 import unittest
+import tree_util
 
 class TreeUtilTest(unittest.TestCase):
+	def setUp(self):
+		self.root = tree_util.Node({'id':0, 'form':'improve'})
+		self.root.add_children([
+			tree_util.Node({'id':1, 'form':'economy'}),
+			tree_util.Node({'id':2, 'form':'to'}),
+			])
+	
 	def test_search_node(self):
-		pass
+		testcases = [('id', 2), ('form', 'economy'), ('pos', 1), ('form', 'one')]
+		self.assertEqual(self.root.find_child('id', 2).label['id'], 2)
+		self.assertEqual(self.root.find_child('form', 'economy').label['form'], 'economy')
+		self.assertIsNone(self.root.find_child('pos', 1))
+		self.assertIsNone(self.root.find_child('form', 'one'))
 
 if __name__ == '__main__':
 	unittest.main()
