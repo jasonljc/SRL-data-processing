6e3408c90baea646fafd2477b9fa3ce23b7a2ddf
Jason Lin <jason.lin.sjtu@gmail.com>
2018-03-02 18:54:02 +0000

    loading word parameters

--- coref_resolution.py
+++ coref_resolution.py
@@ -3,6 +3,7 @@ import xml.etree.ElementTree as ET
 class CoreNLPOutputNotLoadedException(BaseException):
     pass
 
+
 class CorefResoluter(object):
     def __init__(self):
         self.corefs = None
@@ -14,15 +15,34 @@ class CorefResoluter(object):
     
     def find_corefs(self, sentence, start, end):
         # Find coreferences if file is loaded
-        if not self.corefs:
+        if self.corefs is None:
             print('file not loaded')
             raise CoreNLPOutputNotLoadedException
-            
         for coref in self.corefs.findall('coreference'):
             # A block of coreference
-            for mention in coref.findall('mentions'):
+            for mention in coref.findall('mention'):
                 # Find if any items matches the given arguments.
-                pass
+                if (sentence == int(mention[0].text) and 
+                    start == int(mention[1].text) and
+                    end == int(mention[2].text)):
+                    break
+            else:
+                continue
+            
+            # Found coreference
+            ret = []
+            for mention in coref.findall('mention'):
+                if not (sentence == int(mention[0].text) and 
+                    start == int(mention[1].text) and
+                    end == int(mention[2].text)):
+                    ret.append(mention[0].text,
+                               mention[0].text,
+                               mention[0].text)
+            return ret
+                        
+                        
+                
+            import pdb; pdb.set_trace()
 
 def main():
     c = CorefResoluter()
--- srl_processing.py
+++ srl_processing.py
@@ -3,38 +3,63 @@ Basic implementation of SRL processing tasks discussed on Feb 16.
 '''
 
 import tree_builder as tb
-t = tb.TreeBuilder()
+import coref_resolution as cr
+
+def file_loader(filename, col=0):
+    '''
+    Read certain coloumn from text file.
+    '''
+    with open(filename) as fp:
+        ret = fp.readlines()
+        ret = [line.split()[col] for line in ret]
+        return ret
+    return None
 
 base_forms = []
 stative_verbs = []
 possesive_pronouns = []
+actors = []
 
 
-# process single file
-
-t.filename = 'newsText1.txt.srl'
-
-for p in t.get_paragraph_from_file():
-    # process single paragraph
+def main():
     
-    r = t.build_tree(p)
-    state_node = r.find_list('form', base_forms)
-    if not state_node:
-        print 'no state found'
-        continue
-    stative_verb_node = state_node.find_list('form', stative_verbs)
-    if not stative_verb_node:
-        print 'no stative verb found'
-        continue
-    possesive_pronoun_node = stative_verb_node.find_list('form', possesive_pronouns)
-    if not possesive_pronouns:
-        print 'no possesive pronoun found'
-        # find actor directly.
+    base_forms = file_loader('params/states.txt')
+    stative_verbs = file_loader('params/stativewords.txt')
+    
+    actors = file_loader('params/countr_map.txt')
+    
+    t = tb.TreeBuilder()
+    c = cr.CorefResoluter()
+    
+    # process single file
+    
+    t.filename = 'data/newsText1.txt.srl'
+    c.load_file('data/newsText1.xml')
+    
+    for p in t.get_paragraph_from_file():
+        # process single paragraph
         
+        r = t.build_tree(p)
+        state_node = r.find_list('form', base_forms)
+        if not state_node:
+            print 'no state found'
+            continue
+        stative_verb_node = state_node.find_list('form', stative_verbs)
+        if not stative_verb_node:
+            print 'no stative verb found'
+            continue
+        possesive_pronoun_node = stative_verb_node.find_list('form', possesive_pronouns)
+        if not possesive_pronouns:
+            print 'no possesive pronoun found'
+            # find actor directly.
+            
+            
+        else:
+            # find actor through corenlp.
+            pass
         
-    else:
-        # find actor through corenlp.
-        pass
-    
-    # return actor, state, stative verb
-    
+        # return actor, state, stative verb
+
+
+if __name__ == '__main__':
+	main()
